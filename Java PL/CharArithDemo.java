4e2d1a1f9fcd42be0745339441a8350329ac0848
Sanat Sharma <121346520+SanatSharmaa@users.noreply.github.com>
2023-09-04 21:22:05 +0530

    Add files via upload

---
+++ Java PL/CharArithDemo.java
@@ -0,0 +1,14 @@
+public class CharArithDemo {
+    public static void main (String args[]){
+        char ch;
+
+        ch = 'X';
+        System.out.println("ch contains " + ch);
+
+        ch++; //increment ch
+        System.out.println("ch is now "+ ch);
+
+        ch = 90;//gives 
+        System.out.println("ch is now "+ ch);
+    }
+}
